52ce50bdfb6f1d28b81d4b89fff91a42e5dbbbd4
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-10-08 11:47:57 +0100

    add code for cancel shell func

---
+++ shell-scripts/cancel.sh
@@ -0,0 +1,35 @@
+function cancel {
+   item_count=$(sacct --format=state |grep "PENDING\|RUNNING" |wc -l)
+   if (( item_count == 0 )); then
+           tput setaf 3; tput bold; printf "No PENDING or RUNNING jobs\n"; tput sgr0; return 1
+   fi
+
+   # Get IDs and names of all jobs running and queuing     
+   data_col=$(sacct --format=jobid,jobname%40,state |tail -n+3 |grep -Fv '.' |grep "PENDING\|RUNNING" |awk '{print $1}')
+   read -a ids_arr <<< $data_col
+
+   data_col=$(sacct --format=jobid,jobname%40,state |tail -n+3 |grep -Fv '.' |grep "PENDING\|RUNNING" |awk '{print $2}')
+   read -a names_arr <<< $data_col
+
+
+   # printe the id-name of each job
+   tput smul; tput bold; printf "%10s %25s\n" "ID" "NAME     "; tput sgr0; tput rmul;
+   for (( i=0; i<${#names_arr[@]}; i++ )); do
+           printf "%3s %10s %20s\n" $i ${ids_arr[$i]} ${names_arr[$i]}
+   done
+
+   printf "Insert index of job: "; read job_indx
+
+   re='^[0-9]+$'
+   if ! [[ $job_indx =~ $re ]] ; then
+           tput setaf 1; tput bold; printf "ERROR: Not a number or positive integer\n"; tput sgr0; return 1
+   fi
+
+   # Check that the idx is in range [0,n]
+   if [ "$job_indx" -ge "${#ids_arr[@]}" ]; then
+           tput setaf 1; tput bold; printf "ERROR: Index out of bounds\n"; tput sgr0; return 1
+   fi
+
+   printf "running: "; tput bold; tput setaf 3; printf "scancel ${ids_arr[$job_indx]}\n"; tput sgr0
+   scancel ${ids_arr[$job_indx]}
+}
