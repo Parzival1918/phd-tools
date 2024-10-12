d2aa901cfe00f077492cdceb1022e8fc2d063c26
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-10-12 23:07:08 +0100

    improve new-csp script to allow Z'>1

--- shell-scripts/create_config.sh
+++ shell-scripts/create_config.sh
@@ -1,4 +1,12 @@
 function create-config {
     # create a .txt file that contains the default settings
     # in env variables for the new-csp and new-thresh funcs.
+
+    HELP_STR="USAGE: create-config [TYPE]
+
+ARGUMENTS:
+	TYPE csp/thresh: Which type of configuration to create. 
+
+by Pedro Juan Royo
+	"
 }
--- shell-scripts/new_csp.sh
+++ shell-scripts/new_csp.sh
@@ -1,11 +1,11 @@
 function new-csp {
 	# function to create the dir structure of a new CSP run
 
-	HELP_STR="USAGE: new-csp [FOLDER_NAME] [MOLECULE_XYZ]
+	HELP_STR="USAGE: new-csp [FOLDER_NAME] [MOLECULE_XYZ ...]
 
 ARGUMENTS:
 	FOLDER_NAME:  Name of the folder that will be created containing the project.
-	MOLECULE_XYZ:  Path to the initial molecule .xyz file
+	MOLECULE_XYZ:  Path to the initial molecule .xyz file(s)
 
 by Pedro Juan Royo
 	"
@@ -17,14 +17,49 @@ by Pedro Juan Royo
 	FOLDER_6="6-reoptimise"
 
 	PROJ_FOLDER=${1:?"${HELP_STR}"}
-	MOLECULE_XYZ=${2:?"${HELP_STR}"}
-	MOLECULE=${MOLECULE_XYZ%.xyz}
-	EXTENSION=${MOLECULE_XYZ##*.}
 
-	if [ ${EXTENSION} != "xyz" ]; then
-		echo "ERROR: Extension of '${MOLECULE_XYZ}' is not .xyz"
+	if [ -d "${PROJ_FOLDER}" ]; then
+		tput bold; tput setaf 1; echo -n "ERROR:"; tput sgr0; echo " Folder with name '${PROJ_FOLDER}' already exists"
 		return 1
 	fi
+
+	if [ -f "${PROJ_FOLDER}" ]; then
+		tput bold; tput setaf 1; echo -n "ERROR:"; tput sgr0; echo " Folder with name '${PROJ_FOLDER}' cannot be created as file with same name exists"
+		return 1
+	fi
+
+	if [ $# -lt 2 ]; then
+		echo "${HELP_STR}"
+		return 1
+	fi
+
+	MOLECULES=("${@:2}")
+	for M in "${MOLECULES[@]}"; do
+		if [ ! -f "${M}" ]; then
+			tput bold; tput setaf 1; echo -n "ERROR:"; tput sgr0; echo " No file named '${M}' found"
+			return 1
+		fi
+
+		EXTENSION=${M##*.}
+		if [ ${EXTENSION} != "xyz" ]; then
+			tput bold; tput setaf 1; echo -n "ERROR:"; tput sgr0; echo " Extension of '${M}' is not .xyz"
+			return 1
+		fi
+	done
+
+	declare -A uniq_tmp
+
+	for M in "${MOLECULES[@]}"; do
+		uniq_tmp[$M]=0 # assigning a placeholder
+	done
+
+	OPT_XY_STRING=""
+	G09_STRING=""
+	for M in "${!uniq_tmp[@]}"; do
+		m=${M%.xyz}
+		G09_STRING+="g09 ${m}.com ${m}.log; "
+		OPT_XY_STRING+="python opt_xyz_extractor_gaussian.py ${m}.log ${m}.xyz; "
+	done
 
 	# check if csp_settings.txt file exists and source it
 	if [ -f "csp_settings.txt" ]; then
@@ -47,7 +82,7 @@ by Pedro Juan Royo
 
 	# replace vars to string contents of files
 	GAUSSIAN_JOB_SCRIPT="#!/bin/bash
-#SBATCH --job-name=${MOLECULE}_gauss
+#SBATCH --job-name=${PROJ_FOLDER}_gauss
 #SBATCH --mincpus=${GAUSSIAN_CPUS}
 #SBATCH --nodes=1-1
 #SBATCH --ntasks=1
@@ -61,13 +96,13 @@ export GAUSS_SCRDIR=/scratch/\$USER
 export g09root=/iridisfs/i6software/gaussian
 source \$g09root/g09/bsd/g09.profile
 
-g09 ${MOLECULE}.com ${MOLECULE}.log
+${G09_STRING}
 
 # extract optimized coords from log file
 source ~/.bashrc
 conda activate cspy
 
-python opt_xyz_extractor_gaussian.py PYRENE01.log PYRENE01.xyz
+${OPT_XY_STRING}
 	"
 	EXTRACT_OPT_XYZ="from cspy.chem import Molecule
 import argparse
@@ -97,7 +132,7 @@ mol.to_xyz_file(args.output_xyz_file)
 %nprocshared=${GAUSSIAN_CPUS}
 # ${GAUSSIAN_FUNCTIONAL}/${GAUSSIAN_BASIS_SET} opt NoSymm EmpiricalDispersion=GD3BJ
 
-Geometry optimisation calculation for ${MOLECULE}
+Geometry optimisation calculation for ${PROJ_FOLDER}
 
 ${GAUSSIAN_CHARGE} ${GAUSSIAN_MULTIPLICITY}"
 	DMA_ANALYSIS_SCRIPT="#!/bin/bash
@@ -110,7 +145,7 @@ export NUMEXPR_NUM_THREADS=1
 
 ln -fs ../${FOLDER_1}/*.xyz ./
 
-cspy-dma *.xyz
+cspy-dma ${MOLECULES[@]}
 	"
 	CSP_JOB_SCRIPT="#!/bin/bash
 #SBATCH --job-name=${MOLECULE}_csp
@@ -132,11 +167,11 @@ ln -fs ../${FOLDER_2}/*.dma ./
 ln -fs ../${FOLDER_2}/*.mols ./
 
 shopt -s extglob
-mpiexec csp *.xyz -c *_rank0.dma -m !(*_rank0).dma -a *.mols -g fine10
+mpiexec csp ${MOLECULES[@]} -c *_rank0.dma -m !(*_rank0).dma -a *.mols -g fine10
 shopt -u extglob	
 	"
 	REOPTIMIZE_JOB_SCRIPT="#!/bin/bash
-#SBATCH --job-name=${MOLECULE}_reop
+#SBATCH --job-name=${PROJ_FOLDER}_reop
 #SBATCH --partition=batch
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=128
@@ -155,7 +190,7 @@ ln -fs ../${FOLDER_2}/*.dma ./
 ln -fs ../${FOLDER_2}/*.mols ./
 
 shopt -s extglob
-mpiexec cspy-reoptimize output.db -x *.xyz -c *_rank0.dma -m !(*_rank0).dma -a *.mols -p fit --cutoff 30
+mpiexec cspy-reoptimize output.db -x ${MOLECULES[@]} -c *_rank0.dma -m !(*_rank0).dma -a *.mols -p fit --cutoff 30
 shopt -u extglob	
 	"
 	REMOVE_DUPLICATES_SCRIPT="#!/bin/bash
@@ -209,16 +244,6 @@ plt.savefig('Landscape.png', dpi=600)
 print(\"Landscape saved to Landscape.png\")
 	" # inspired from https://mol-cspy.readthedocs.io/en/latest/bb_wikipages/Scripts%20for%20CSPy.html
 
-	if [ -d "${PROJ_FOLDER}" ]; then
-		echo "ERROR: Folder with name '${PROJ_FOLDER}' already exists"
-		return 1
-	fi
-
-	if [ ! -f "${MOLECULE_XYZ}" ]; then
-		echo "ERROR: No file named '${MOLECULE_XYZ}' found"
-		return 1
-	fi
-
 	# create project folder
 	mkdir ${PROJ_FOLDER}
 	cd ${PROJ_FOLDER}
@@ -227,11 +252,16 @@ print(\"Landscape saved to Landscape.png\")
 	mkdir ${FOLDER_1} ${FOLDER_2} ${FOLDER_3} ${FOLDER_4} ${FOLDER_5} ${FOLDER_6}
 
 	# create contents of FOLDER_1
-	cp ../${MOLECULE_XYZ} ${FOLDER_1}/${MOLECULE}.xyz.original
-	echo "${GAUSSIAN_INPUT_FILE}" > ${FOLDER_1}/${MOLECULE}.com
-	tail -n +3 ../${MOLECULE_XYZ} >> ${FOLDER_1}/${MOLECULE}.com
-	echo "" >> ${FOLDER_1}/${MOLECULE}.com # adding two empty lines at end for format requirements
-	echo "" >> ${FOLDER_1}/${MOLECULE}.com
+	for M in "${!uniq_tmp[@]}"; do # copy original xyz files
+		cp ../${M} ${FOLDER_1}/${M}.original
+	done
+	for M in "${!uniq_tmp[@]}"; do # create g09 .com input files for each
+		m=${M%.xyz} # molecule file name without .xyz extension
+		echo "${GAUSSIAN_INPUT_FILE}" > ${FOLDER_1}/${m}.com
+		tail -n +3 ../${M} >> ${FOLDER_1}/${m}.com
+		echo "" >> ${FOLDER_1}/${m}.com # adding two empty lines at end for format requirements
+		echo "" >> ${FOLDER_1}/${m}.com
+	done
 	echo "${EXTRACT_OPT_XYZ}" > ${FOLDER_1}/opt_xyz_extractor_gaussian.py
 	echo "${GAUSSIAN_JOB_SCRIPT}" > ${FOLDER_1}/job_submit.sh
 	echo "# ${FOLDER_1}
@@ -314,8 +344,13 @@ accurate energy methods.
 	" > README.md 
 	cd ..
 
+	# create project.info file
+	local project_info="project.info"
+	echo "PROJ_NAME=${PROJ_FOLDER}" > ${project_info}
+	echo "PROJ_LOCATION=${PWD}" >> ${project_info}
+
 	# return to initial folder
 	cd ..
 
-	echo "Created the project in '${PROJ_FOLDER}'" 
+	tput bold; tput setaf 3; echo "Created the project in '${PROJ_FOLDER}'"; tput sgr0 
 }
