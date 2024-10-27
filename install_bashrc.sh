7ed8e54c28af235871ba0e98f27d782c2b1d7546
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-10-27 14:49:17 +0000

    shell script to add managed code to bashrc

---
+++ install_bashrc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if grep -q ">>>phd-tools managed block START<<<" ~/.bashrc; then
+    tput bold; tput setaf 1; echo -n "ERROR:"; tput sgr0; echo " .bashrc already contains the managed source files"
+    exit 1
+fi
+
+tput bold; tput setaf 3; echo "Adding to .bashrc sourcing of phd-tools"; tput sgr0
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+bashrc_text="
+>>>phd-tools managed block START<<<
+source $SCRIPT_DIR/shell-scripts/add_to_bashrc.sh
+>>>phd-tools managed block END<<<
+"
+
+echo $bashrc_text >> ~/.bashrc
